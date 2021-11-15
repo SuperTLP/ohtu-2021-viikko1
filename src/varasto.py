a18111a78e490ad32ebca42bbab25bc8b4dafa84
Riku Mattila <riku.p.mattila@gmail.com>
2021-11-15 17:34:45 +0200

    rikotaan testi

--- src/varasto.py
+++ src/varasto.py
@@ -1,7 +1,11 @@
 class Varasto:
     def __init__(self, tilavuus, alku_saldo = 0):
-        j = tilavuus>0
-        self.tilavuus={False: 0.0, True: tilavuus}[j]
+        # j = tilavuus>0
+        # self.tilavuus={False: 0.0, True: tilavuus}[j]
+        if tilavuus>0:
+            self.tilavuus=tilavuus
+        else:
+            self.tilavuus=0.0
         if alku_saldo < 0.0:
             # virheellinen, nollataan
             self.saldo = 0.0
